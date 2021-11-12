fa894d95b856309bf727580930d5330db04603e5
Guilherme Feitosa <guifeitos@gmail.com>
2021-11-12 13:06:32 -0300

    criação arquitetura MVVM

---
+++ Desafio_CoreData/Desafio_CoreData/View/PeopleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PeopleTableViewCell.swift
+//  Desafio_CoreData
+//
+//  Created by Gui Feitosa on 12/11/21.
+//
+
+import UIKit
+
+class PeopleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
