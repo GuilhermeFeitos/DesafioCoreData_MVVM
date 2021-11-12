8bcf0419ddd72c902b4b07ce7a14ba51011dbf93
Guilherme Feitosa <guifeitos@gmail.com>
2021-11-12 13:16:15 -0300

    Criação da PeopleViewModel + Criação do Entity People no CoreData + Criacao do Model People

---
+++ Desafio_CoreData/Desafio_CoreData/Model/People+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  People+CoreDataClass.swift
+//  Desafio_CoreData
+//
+//  Created by Gui Feitosa on 12/11/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(People)
+public class People: NSManagedObject {
+
+}
